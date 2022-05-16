28a438cbaaaa8859ce0b7d7b20b2697b3fe93ee6
Maximo <63942620+maxfer1221@users.noreply.github.com>
2022-05-16 01:51:49 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@
 export SPOTIPY_USERNAME='{username}'
 export SPOTIPY_CLIENT_ID='{client id}'
 export SPOTIPY_CLIENT_SECRET='{client secret}'
+export SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
 
 sudo -E python3 test.py
